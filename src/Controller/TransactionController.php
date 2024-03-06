0dc4f14a5060f5616d72de164bfd1cdc493c3296
Koretech <oudart.aurelien@live.fr>
2024-03-06 23:14:22 +0100

    Add Transaction creation
    - Add Transaction creation in listOrCreate method of Transaction
    controller
    - Display form in index_or_create view of Transaction entity

--- src/Controller/TransactionController.php
+++ src/Controller/TransactionController.php
@@ -3,17 +3,46 @@
 namespace App\Controller;
 
 use App\Entity\Transaction;
+use App\Form\TransactionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TransactionController extends AbstractController
 {
-    #[Route('/transaction', name: 'transaction_index_or_create')]
-    public function listOrCreate(): Response
+    public function __construct(private EntityManagerInterface $em)
     {
-        return $this->render('transaction/index_or_create.html.twig');
+    }
+
+    #[Route('/transaction', name: 'transaction_index_or_create')]
+    public function listOrCreate(Request $request): Response
+    {
+        $transaction = new Transaction();
+        $newTransactionForm = $this->createForm(TransactionType::class, $transaction);
+        $newTransactionForm->handleRequest($request);
+
+        if ($newTransactionForm->isSubmitted() && $newTransactionForm->isValid()) {
+            $transaction = $newTransactionForm->getData();
+            $debit = $newTransactionForm['debit']->getData();
+            $credit = $newTransactionForm['credit']->getData();
+
+            if (!is_null($debit)) {
+                $this->transformDebitIntoValue($transaction, $debit);
+            }
+            if (!is_null($credit)) {
+                $this->transformCreditIntoValue($transaction, $credit);
+            }
+
+            $this->em->persist($transaction);
+            $this->em->flush();
+        }
+
+        return $this->render('transaction/index_or_create.html.twig', [
+            'newTransactionForm' => $newTransactionForm->createView()
+        ]);
     }
 
     #[Route('/transaction/edit/{id}', name: 'transaction_edit', requirements: ["id" => "\d+"])]
@@ -26,5 +55,21 @@ class TransactionController extends AbstractController
     public function delete(Transaction $transaction): RedirectResponse
     {
         return $this->redirectToRoute('transaction_index_or_create');
+    }
+
+    private function transformDebitIntoValue(Transaction $transaction, float $value): void
+    {
+        $transaction
+            ->setType(0)
+            ->setValue($value)
+        ;
+    }
+
+    private function transformCreditIntoValue(Transaction $transaction, float $value): void
+    {
+        $transaction
+            ->setType(1)
+            ->setValue($value)
+        ;
     }
 }
