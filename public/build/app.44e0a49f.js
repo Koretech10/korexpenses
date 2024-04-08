5fc776eb7a4fd491fe272f0473e6dbaf99cbc595
Koretech <oudart.aurelien@live.fr>
2024-04-08 13:13:49 +0200

    Initialize bootstrap tooltips

---
+++ public/build/app.44e0a49f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[524],{28:(t,e,a)=>{var o=a(692);a(851),a(754),o(document).ready((function(){o('[data-toggle="tooltip"]').tooltip()}))}},t=>{t.O(0,[754],(()=>{return e=28,t(t.s=e);var e}));t.O()}]);
