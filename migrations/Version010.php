e8aff2ba04ba70979a83b6eb4227e4a2b77b7875
Koretech <oudart.aurelien@live.fr>
2024-04-08 22:51:14 +0200

    Disable messenger package
    - Delete messenger.yaml
    - Remove messenger environment variable in .env
    - Remove messenger table in Version010 migration

--- migrations/Version010.php
+++ migrations/Version010.php
@@ -52,22 +52,6 @@ final class Version010 extends AbstractMigration
             ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         ');
         $this->addSql('
-            CREATE TABLE messenger_messages (
-                id BIGINT AUTO_INCREMENT NOT NULL,
-                body LONGTEXT NOT NULL,
-                headers LONGTEXT NOT NULL,
-                queue_name VARCHAR(190) NOT NULL,
-                created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\',
-                available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\',
-                delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\',
-                INDEX IDX_75EA56E0FB7336F0 (queue_name),
-                INDEX IDX_75EA56E0E3BD61CE (available_at),
-                INDEX IDX_75EA56E016BA31DB (delivered_at),
-                PRIMARY KEY(id)
-            ) 
-            DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
-        ');
-        $this->addSql('
             ALTER TABLE transaction
             ADD CONSTRAINT FK_723705D19B6B5FBA FOREIGN KEY (account_id) REFERENCES account (id) ON DELETE CASCADE
         ');
@@ -82,7 +66,6 @@ final class Version010 extends AbstractMigration
         ');
         $this->addSql('DROP TABLE account');
         $this->addSql('DROP TABLE transaction');
-        $this->addSql('DROP TABLE messenger_messages');
         $this->addSql('DROP TABLE user');
     }
 }
