5e4a7e62eca996f30c8f4625ddfef0b406672cd0
Aurélien OUDART <aoudart@hexanet.fr>
2024-06-18 17:29:27 +0200

    Add MonthlyTransaction creation

--- src/Controller/MonthlyTransactionController.php
+++ src/Controller/MonthlyTransactionController.php
@@ -4,25 +4,51 @@ namespace App\Controller;
 
 use App\Entity\Account;
 use App\Entity\MonthlyTransaction;
+use App\Form\MonthlyTransactionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MonthlyTransactionController extends AbstractController
 {
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+    ) {
+    }
+
     /**
      * Lister les opérations mensuelles
      * @param Account|null $account
      * @param int $page
+     * @return RedirectResponse|Response
      */
     #[Route(
         '/transaction/monthly/list/{account}/{page}',
         name: 'monthly_transaction_list',
         requirements: ['account' => '\d+', 'page' => '\d'],
     )]
-    public function list(Account $account = null, int $page = 1)
+    public function list(Account $account = null, int $page = 1): RedirectResponse|Response
     {
+        // Vérification de l'existence du compte bancaire demandé
+        if (!$account) {
+            $this->addFlash('danger', "Ce compte n'existe pas.");
+            return $this->redirectToRoute('account_list');
+        }
+
+        // Formulaire de création d'opération mensuelle
+        $monthlyTransaction = new MonthlyTransaction();
+        $newMonthlyTransactionForm = $this->createForm(MonthlyTransactionType::class, $monthlyTransaction, [
+            'target_url' => $this->generateUrl('monthly_transaction_create', [
+                'account' => $account->getId(),
+            ])
+        ]);
+
+        return $this->render('monthly_transaction/list.html.twig', [
+            'newMonthlyTransactionForm' => $newMonthlyTransactionForm->createView(),
+        ]);
     }
 
     /**
@@ -44,14 +70,42 @@ class MonthlyTransactionController extends AbstractController
      * Créer une opération mensuelle
      * @param Request $request
      * @param Account|null $account
+     * @return RedirectResponse
      */
     #[Route(
         '/transaction/monthly/create/{account}',
         name: 'monthly_transaction_create',
         requirements: ['account' => '\d+']
     )]
-    public function create(Request $request, Account $account = null)
+    public function create(Request $request, Account $account = null): RedirectResponse
     {
+        // Vérification de l'existence du compte bancaire demandé
+        if (!$account) {
+            $this->addFlash('danger', "Ce compte n'existe pas.");
+            return $this->redirectToRoute('account_list');
+        }
+
+        // Récupération de formulaire de création
+        $monthlyTransaction = new MonthlyTransaction();
+        $monthlyTransaction->setAccount($account);
+        $newMonthlyTransactionForm = $this->createForm(MonthlyTransactionType::class, $monthlyTransaction, [
+            'target_url' => $this->generateUrl('monthly_transaction_create', [
+                'account' => $account->getId(),
+            ])
+        ]);
+        $newMonthlyTransactionForm->handleRequest($request);
+
+        if ($newMonthlyTransactionForm->isSubmitted() && $newMonthlyTransactionForm->isValid()) {
+            $monthlyTransaction = $newMonthlyTransactionForm->getData();
+
+            $this->em->persist($monthlyTransaction);
+            $this->em->flush();
+            $this->addFlash('success', "L'opération mensuelle a été créée avec succès.");
+        }
+
+        return $this->redirectToRoute('monthly_transaction_list', [
+            'account' => $account->getId(),
+        ]);
     }
 
     /**
