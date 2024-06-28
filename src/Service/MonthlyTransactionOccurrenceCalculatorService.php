764f19d8555cdc345f100ccd7326c046a6b87cb9
Aurélien OUDART <aoudart@hexanet.fr>
2024-06-28 16:10:08 +0200

    Add monthly transaction occurrence calculator service

---
+++ src/Service/MonthlyTransactionOccurrenceCalculatorService.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Service;
+
+/**
+ * Service permettant de calculer la prochaine occurrence d'une opération mensuelle
+ */
+class MonthlyTransactionOccurrenceCalculatorService
+{
+}
