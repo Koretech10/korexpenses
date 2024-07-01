c9bd292eecd2c7ac324b42ddf6070c0a37d1d7df
Koretech <oudart.aurelien@live.fr>
2024-07-01 17:46:36 +0200

    Add remember me functionality

--- src/Form/LoginType.php
+++ src/Form/LoginType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -22,6 +23,10 @@ class LoginType extends AbstractType
             ->add('_password', PasswordType::class, [
                 'label' => 'Mot de passe',
                 'required' => true
+            ])
+            ->add('_remember_me', CheckboxType::class, [
+                'label' => "Se souvenir de moi",
+                'required' => false,
             ])
             ->add('login', SubmitType::class, [
                 'label' => 'Se connecter'
